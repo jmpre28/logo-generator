118401943c455dafe64817eebdb813135e9d492f
Jake Preciado <jacobpreciado@gmail.com>
2023-09-18 18:37:05 -0600

    restructured the repo directories, setup classes and their inheritance

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
-inquirer
-    prompt 
+const fs = require('fs');
+const inquirer = require('inquirer');
+const shape = require('./lib/shapes');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for your logo'
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Now choose a color for the text of your logo'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Select a shape for your logo',
+        choices: ['Triangle', 'Circle', 'Square']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Now select a color for the shape of your logo.'
+    }
+];
+
+
+function renderLogo(data) {
+    let shape = None;
+    if(data.shape === 'Triangle') {
+        const shape = new Triangle(data.text, data.textColor, data.shapeColor);
+    } else if (data.shape === 'Square') {
+        const shape = new Square(data.text, data.textColor, data.shapeColor);
+    } else if (data.shape === 'Circle') {
+        const shape = new Circle(data.text, data.textColor, data.shapeColor);
+    }
+    return shape.render();
+};
+
+function init() {
+    inquirer
+        .createPromptModule(questions)
+        .then((data) => {
+            const fileName = 'logo.svg';
+            fs.writeFile(fileName, data);
+        });
+};
+
+init();
